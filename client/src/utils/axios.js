b2066b8b668ae95b25d75f39e29f089f5b686484
aditi kumari <aditithakur0090@gmail.com>
2026-06-01 13:01:42 +0530

    updated axios base URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://social-1-l0tk.onrender.com'
 });
 
 api.interceptors.request.use(
